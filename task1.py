05e2a34d84c0f4f6d2be806d8d7bf1f3c799b42a
safa403 <safapalakkal383@gmail.com>
2025-09-10 06:58:49 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,21 @@
+import unittest
+
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-1, 5), -5)
+        self.assertEqual(multiply(0, 10), 0)
+
+if __name__ == "__main__":
+    unittest.main()
